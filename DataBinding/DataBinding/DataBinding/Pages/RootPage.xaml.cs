d17c2e91296bfa39f376b439aa243f54f5891b49
Bob Zurad <bob@zurad.net>
2016-12-27 22:18:55 -0600

    DataBinding project

---
+++ DataBinding/DataBinding/DataBinding/Pages/RootPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DataBinding.Pages
+{
+    public partial class RootPage : TabbedPage
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataBinding/DataBinding/DataBinding/Pages/SliderTransformsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DataBinding.Pages
+{
+    public partial class SliderTransformsPage : ContentPage
+    {
+        public SliderTransformsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
